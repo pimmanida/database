ae3393ed56e5ed3e2b8de86cb75142b0e62d96cc
Suprawich <112562696+Suprawich@users.noreply.github.com>
2023-04-16 23:52:23 +0700

    header

--- app_m/header.php
+++ app_m/header.php
@@ -1,5 +1,58 @@
 <header class="header">
     <div class="flex">
-        <img src="img/SAHARA.svg" alt="">
+        <div class="sec1">
+            <img src="img/SAHARA.svg" alt="">
+            <nav class="navbar">
+                <a href="#">Home</a>
+                <a href="#">Menu</a>
+                <a href="#">My order</a>
+            </nav>
+        </div>
+        <div class="sec2">
+
+        </div>
     </div>
 </header>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap');
+
+
+    header {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .flex {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 18px 40px;
+        position: relative;
+    }
+
+    .sec1 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0px;
+        gap: 40px;
+    }
+
+    nav {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 25px;
+    }
+
+    nav a {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 21px;
+        color: #784A14;
+        text-decoration: none;
+    }
+</style>
--- app_m/reservation.php
+++ app_m/reservation.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+</head>
 <body>
     <?php include 'header.php'; ?>
 </body>
