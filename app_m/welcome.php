2c7cc00dc34c512f5ca7757e87de8fa64cb80853
pimmanida <ppimmanidaa@gmail.com>
2023-04-18 00:52:16 +0700

    eekrob

--- app_m/welcome.php
+++ app_m/welcome.php
@@ -18,7 +18,7 @@
 <body>
 
     <div class="container">
-        <?php echo $_SESSION['uname']; ?></h1>
+        <?php echo $_SESSION['fname']; ?></h1>
         <hr>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa vel blanditiis soluta aspernatur commodi nobis eaque minima eum dolor, facere debitis consequuntur nesciunt! Ratione dolores labore aspernatur commodi, perspiciatis tempore. Officiis nam enim vitae debitis deserunt, placeat voluptates alias, voluptas explicabo ad, earum et maxime. Odit placeat inventore ratione! Accusantium autem esse unde fugit repudiandae optio voluptatibus repellat? Dolorum, magni molestiae adipisci temporibus tempore est maiores sunt nulla nihil ullam doloremque quam cumque harum aut vero delectus earum nam? Accusantium tempora, at voluptatibus reprehenderit sed, optio ducimus earum facilis harum, dicta ab dolorem! Mollitia nobis dolorum fuga illo doloribus nulla sit impedit inventore, reiciendis omnis aspernatur eligendi laborum minima dolorem fugit. Ad maiores, blanditiis nisi nobis similique, voluptas necessitatibus dolorem at esse saepe iste eligendi nam excepturi quidem suscipit repellat cumque. Corporis, dolorum eum? A distinctio libero animi placeat vel odio dolor adipisci non autem? Natus facere nesciunt ullam cum et eaque consectetur quae officia. Eaque voluptatum, amet minima minus reiciendis dolores quidem dolore vero consequatur, quibusdam neque laborum, id voluptatem temporibus! Nisi mollitia veniam, dolores minima unde error exercitationem, nam aspernatur accusantium sapiente ut fugit tempore at corrupti nesciunt, accusamus ea? At natus perferendis, molestias beatae animi nam cumque.
